70e54dc89a04543e56413830392e7c3696249947
LinmeiJ <linmeijiang@hotmail.com>
2019-06-17 11:27:28 -0400

    added jpa dependency

---
+++ src/main/java/com/petmatch/PetMatch/pojos/petType.java
@@ -0,0 +1,19 @@
+package com.petmatch.PetMatch.pojos;
+
+import javax.persistence.Entity;
+
+@Entity
+public class petType {
+	
+	private Integer petid;
+	private String petType;
+	private String keywords;
+	
+	public petType() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
