531d08c3bf127812f85e503f9733dd4d7f40cc56
LinmeiJ <linmeijiang@hotmail.com>
2019-06-19 13:35:49 -0400

    fix mergin problem

--- src/main/java/com/petmatch/PetMatch/DBservice/DataFromDB.java
+++ src/main/java/com/petmatch/PetMatch/DBservice/DataFromDB.java
@@ -2,9 +2,13 @@ package com.petmatch.PetMatch.DBservice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,62 +19,77 @@ import com.petmatch.PetMatch.repo.PetsRepo;
 
 @Component
 public class DataFromDB {
-	
-	@Autowired
-	PetsRepo pr;
-		
-	//store the types from pets database into a list 
-	public List<String> storeTypes(){
-		List<String> petTypeDB = pr.getType();
-		return petTypeDB;
-	} 
-	
-	//store the keywords from pets database into a list
-	public List<String> storeKeywords(){
-		List<String> keywordsList = pr.getKeywords();
-		return keywordsList; 
-	}
-	
-	//check match 	
-	public Map<String, Integer> storeMatchInHashMap(String space, String interact, String cost, String hours, String time) {
-		
-		Map<String, Integer> matchRate = new HashMap<>();
-		checkMatch(storeInputsIntoAList(space, interact, cost, hours, time), matchRate);
-		
-		return matchRate; 
-	}
 
-	//store user inputs into a List, return the list
-	private List<String> storeInputsIntoAList(String space, String interact, String cost, String hours, String time) {
-		List<String>  ls = new ArrayList<>(Arrays.asList(space, interact, cost, hours, time));
-		return ls;
-	}
+    @Autowired
+    PetsRepo pr;
 
-	
-	private void checkMatch(List<String> ls, Map<String, Integer> matchRate) {
-		
-			for(int i = 0; i < storeKeywords().size(); i++) { 
-				
-				if(storeKeywords().get(i).contains("FIX ME") { 
-					
-					if(matchRate.containsKey(storeTypes().get(i))) {//if the hashmap has the key/type
-						incrementValue(matchRate, storeTypes().get(i)); 
-					}
-					else{
-						matchRate.put(storeTypes().get(i), 1); 
-						}
-				}
-			}
-	}
-	
-	//if the type is already in the hash-map(in above method tells details), then just increment the its value
-	public void incrementValue(Map<String, Integer> map, String k) {
-		Integer count = map.get(k);  //get the value of this certain key. remember the values in hash-map are Integer type
-		if(count == null) { //just double check in case the key doesn't have a value assigned in 
-			count = 0;//give a value so we can increment by adding 1（check next line）
-		}
-		map.put(k, count + 1); //use the same key. but add its value by 1.
-	}
-	
-	
+    // store the types from pets database into a list
+    public List<String> storeTypes() {
+        List<String> petTypeDB = pr.getType();
+        return petTypeDB;
+    }
+
+    // store the keywords from pets database into a list
+    public List<String> storeKeywords() {
+        List<String> keywordsList = pr.getKeywords();
+        return keywordsList;
+    }
+
+    // store user inputs into a List, return the list
+    private List<String> storeInputsIntoAList(String space, String interact, String cost, String hours, String noise) {
+        List<String> ls = new ArrayList<>(Arrays.asList(space, interact, cost, hours, noise));
+        return ls;
+    }
+    
+    //sorted their value for display the bars in order
+    // store the sorted match result to hashMap
+    public  Map<String, Integer> storeMatchInHashMap(String space, String interact, String cost, String hours,
+            String noise) {
+        Map<String, Integer> matchRate = new HashMap<>();
+        checkMatch(storeInputsIntoAList(space, interact, cost, hours, noise), matchRate);        
+        
+        //sorted it by its value
+        Map<String, Integer> valueSortedMap = valueSortHashMapValue(matchRate);
+        return valueSortedMap;
+    }
+
+    //sort map value
+    private Map<String, Integer> valueSortHashMapValue(Map<String, Integer> matchRate) {
+    	
+        List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(
+                    matchRate.entrySet());
+        
+                Collections.sort(list, new MyComparator()); //check <ComparetorForSort> Class
+                Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+                for (Entry<String, Integer> entry : list) {
+                  sortedMap.put(entry.getKey(), entry.getValue());
+                }
+              return sortedMap;
+    }
+    
+    
+    
+    //check match by loop through user inputs and keywords string
+    private void checkMatch(List<String> ls, Map<String, Integer> matchRate) {
+
+        for (int i = 0; i < ls.size(); i++) {
+            for (int j = 0; j < storeKeywords().size(); j++) {
+                    if (storeKeywords().get(j).contains(ls.get(i))) {
+                        if (matchRate.containsKey(storeTypes().get(j))) {// if the hashmap has the key/type
+                            incrementValue(matchRate, storeTypes().get(j));
+                        } else {
+                            matchRate.put(storeTypes().get(j), 1);
+                        }
+                    }
+                }
+            }
+    }
+
+    
+    // if the type is already in the hash-map(in above method tells details), then
+    // just increment the its value
+    public void incrementValue(Map<String, Integer> map, String type) { // type is all defined as key value
+        Integer count = map.get(type); // get the value of this certain key.
+        map.put(type, count + 1); // use the same key. but add its value by 1.
+    }
 }
