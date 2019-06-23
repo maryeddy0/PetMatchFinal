aad8614dcf7d723904e95a02dce05ec9ff544faa
LinmeiJ <linmeijiang@hotmail.com>
2019-06-23 15:18:20 -0400

    adjusted pets details page with css

--- src/main/java/com/petmatch/PetMatch/apiService/PetService.java
+++ src/main/java/com/petmatch/PetMatch/apiService/PetService.java
@@ -86,7 +86,7 @@ public class PetService {
 		// check the photo from API is not empty
 		//there is definitely a better way to do the same thing here, I just don't know yet.
 		if (petIDResponse.getBody().getAnimal().getPhotos().isEmpty()) {
-			ssp.setPhotos("https://www.capelmanorcollege.co.uk/wp-content/uploads/2015/12/image-not-yet-available.jpg");
+			ssp.setPhotos("https://smhttp-ssl-50970.nexcesscdn.net/media/catalog/product/cache/1/image/300x/9df78eab33525d08d6e5fb8d27136e95/placeholder/default/no_image_available_3.jpg");
 			ssp = new StoreSelectedPets(ssp.getPhotos(), 
 										petIDResponse.getBody().getAnimal().getName(),
 										petIDResponse.getBody().getAnimal().getAge(),
